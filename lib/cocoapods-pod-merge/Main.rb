5528cee857e8904a73c44db5b4b9c146ab85e223
Victor Sigler <vikt0r.sigler@gmail.com>
2021-04-06 10:34:41 -0400

    Add the Rome logic to produce a *.framework

--- lib/cocoapods-pod-merge/Main.rb
+++ lib/cocoapods-pod-merge/Main.rb
@@ -7,6 +7,7 @@ require 'cocoapods'
 require 'fileutils'
 require 'json'
 require 'digest/md5'
+require 'fourflusher'
 
 CacheDirectory = 'MergeCache'
 InstallationDirectory = 'MergedPods'
@@ -78,6 +79,180 @@ module CocoapodsPodMerge
       mergefile.explicit_file_type = 'text.script.ruby'
       mergefile.include_in_index = '1'
       pods_project.save
+
+      build_framework(installer_context)
+    end
+
+    def exclude_arm64_architectures(project_path, configuration)
+      project = Xcodeproj::Project.open(project_path)
+      project.targets.each do |target|
+        config = target.build_configurations.find { |config| config.name.eql? configuration }
+        config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
+      end
+      project.save
+    end
+
+    def copy_bcsymbolmap_files(symbols_destination, configuration)
+      Pod::UI.puts "Copying the bcsymbolmap files for the generated frameworks."
+      symbols_destination.rmtree if symbols_destination.directory?
+      symbols = Pathname.glob("build/#{configuration}-iphoneos/**/*.bcsymbolmap")
+      symbols.each do |symbol|
+        FileUtils.mkdir_p symbols_destination
+        FileUtils.cp_r symbol, symbols_destination, :remove_destination => true
+      end
+    end
+
+    def enable_debug_information(project_path, configuration)
+      project = Xcodeproj::Project.open(project_path)
+      project.targets.each do |target|
+        config = target.build_configurations.find { |config| config.name.eql? configuration }
+        config.build_settings['DEBUG_INFORMATION_FORMAT'] = 'dwarf-with-dsym'
+        config.build_settings['ONLY_ACTIVE_ARCH'] = 'NO'
+      end
+      project.save
+    end
+    
+    def enable_bitcode(project_path, configuration)
+      project = Xcodeproj::Project.open(project_path)
+      project.targets.each do |target|
+        config = target.build_configurations.find { |config| config.name.eql? configuration }
+        config.build_settings['ENABLE_BITCODE'] = 'YES'
+        config.build_settings['BITCODE_GENERATION_MODE'] = 'bitcode'
+      end
+      project.save
+    end
+    
+    def copy_dsym_files(dsym_destination, configuration)
+      dsym_destination.rmtree if dsym_destination.directory?
+      platforms = ['iphoneos', 'iphonesimulator']
+      platforms.each do |platform|
+        dsym = Pathname.glob("build/#{configuration}-#{platform}/**/*.dSYM")
+        dsym.each do |dsym|
+          destination = dsym_destination + platform
+          FileUtils.mkdir_p destination
+          FileUtils.cp_r dsym, destination, :remove_destination => true
+        end
+      end
+    end
+
+    def copy_dsym_and_bcsymbolmap(project_path)
+      Pod::UI.puts "Copying the bcsymbolmap files & dSYMs to the Framework folder."
+    
+      destination = 'Framework'
+
+      symbols = Pathname.glob("bcsymbolmap/**/*.bcsymbolmap")
+      symbols.each do |symbol|
+        FileUtils.cp_r symbol, destination, :remove_destination => true
+      end
+
+      dSYMs = Pathname.glob("dSYM/iphoneos/**/*.dSYM")
+      dSYMs.each do |dSYM|
+        FileUtils.cp_r dSYM, destination, :remove_destination => true
+      end
+
+      FileUtils.rm_rf(project_path + 'dSYM')
+      FileUtils.rm_rf(project_path + 'bcsymbolmap')
+    end 
+
+    PLATFORMS = { 'iphonesimulator' => 'iOS',
+      'appletvsimulator' => 'tvOS',
+      'watchsimulator' => 'watchOS' }
+
+    def build_for_iosish_platform(sandbox, build_dir, target, device, simulator, configuration)
+      deployment_target = target.platform_deployment_target
+      target_label = target.cocoapods_target_label
+
+      xcodebuild(sandbox, target_label, device, deployment_target, configuration)
+      xcodebuild(sandbox, target_label, simulator, deployment_target, configuration)
+
+      spec_names = target.specs.map { |spec| [spec.root.name, spec.root.module_name] }.uniq
+      spec_names.each do |root_name, module_name|
+        executable_path = "#{build_dir}/#{root_name}"
+        device_lib = "#{build_dir}/#{configuration}-#{device}/#{root_name}/#{module_name}.framework/#{module_name}"
+        device_framework_lib = File.dirname(device_lib)
+        simulator_lib = "#{build_dir}/#{configuration}-#{simulator}/#{root_name}/#{module_name}.framework/#{module_name}"
+
+        next unless File.file?(device_lib) && File.file?(simulator_lib)
+
+        lipo_log = `lipo -create -output #{executable_path} #{device_lib} #{simulator_lib}`
+        puts lipo_log unless File.exist?(executable_path)
+
+        FileUtils.mv executable_path, device_lib, :force => true
+        FileUtils.mv device_framework_lib, build_dir, :force => true
+        FileUtils.rm simulator_lib if File.file?(simulator_lib)
+        FileUtils.rm device_lib if File.file?(device_lib)
+      end
+    end
+
+    def xcodebuild(sandbox, target, sdk='macosx', deployment_target=nil, configuration)
+      args = %W(-project #{sandbox.project_path.realdirpath} -scheme #{target} -configuration #{configuration} -sdk #{sdk})
+      platform = PLATFORMS[sdk]
+      args += Fourflusher::SimControl.new.destination(:oldest, platform, deployment_target) unless platform.nil?
+      Pod::Executable.execute_command 'xcodebuild', args, true
+    end
+
+    def build_framework(installer_context)
+      sandbox_root = Pathname(installer_context.sandbox_root)
+      sandbox = Pod::Sandbox.new(sandbox_root)
+
+      configuration = 'Release'
+      exclude_arm64_architectures(sandbox.project_path, configuration)
+      enable_bitcode(sandbox.project_path, configuration)
+      enable_debug_information(sandbox.project_path, configuration)
+      
+      build_dir = sandbox_root.parent + 'build'
+      destination = sandbox_root.parent + 'Framework'
+
+      Pod::UI.puts "Building the frameworks"
+
+      build_dir.rmtree if build_dir.directory?
+      targets = installer_context.umbrella_targets.select { |t| t.specs.any? }
+      targets.each do |target|
+        case target.platform_name
+        when :ios then build_for_iosish_platform(sandbox, build_dir, target, 'iphoneos', 'iphonesimulator', configuration)
+        else raise "Unknown platform '#{target.platform_name}'" end
+      end
+
+      raise Pod::Informative, 'The build directory was not found in the expected location.' unless build_dir.directory?
+
+      # Make sure the device target overwrites anything in the simulator build, otherwise iTunesConnect
+      # can get upset about Info.plist containing references to the simulator SDK
+      frameworks = Pathname.glob("build/*/*/*.framework").reject { |f| f.to_s =~ /Pods[^.]+\.framework/ }
+      frameworks += Pathname.glob("build/*.framework").reject { |f| f.to_s =~ /Pods[^.]+\.framework/ }
+
+      resources = []
+
+      Pod::UI.puts "Built #{frameworks.count} #{'frameworks'.pluralize(frameworks.count)}"
+
+      destination.rmtree if destination.directory?
+
+      installer_context.umbrella_targets.each do |umbrella|
+        umbrella.specs.each do |spec|
+          consumer = spec.consumer(umbrella.platform_name)
+          pod_dir = sandbox.pod_dir(spec.root.name).parent
+
+          file_accessor = Pod::Sandbox::FileAccessor.new(pod_dir, consumer)
+          frameworks += file_accessor.vendored_libraries
+          frameworks += file_accessor.vendored_frameworks
+          resources += file_accessor.resources
+        end
+      end
+      frameworks.uniq!
+      resources.uniq!
+
+      Pod::UI.puts "Copying #{frameworks.count} #{'frameworks'.pluralize(frameworks.count)} " \
+        "to the `#{destination.relative_path_from Pathname.pwd}` folder"
+
+      FileUtils.mkdir_p destination
+      (frameworks + resources).each do |file|
+        FileUtils.cp_r file, destination, :remove_destination => true
+      end
+
+      copy_dsym_files(sandbox_root.parent + 'dSYM', configuration)
+      copy_bcsymbolmap_files(sandbox_root.parent + 'bcsymbolmap', configuration)
+      copy_dsym_and_bcsymbolmap(sandbox_root.parent)
+
+      build_dir.rmtree if build_dir.directory?
     end
 
     def add_to_gitignore
@@ -89,8 +264,8 @@ module CocoapodsPodMerge
       cache_folder = contents.scan(/#{CacheDirectory}/)
       contents += "\n#{CacheDirectory}/" unless cache_folder&.last
 
-      merged_folder = contents.scan(/#{InstallationDirectory}/)
-      contents += "\n#{InstallationDirectory}/" unless merged_folder&.last
+      #merged_folder = contents.scan(/#{InstallationDirectory}/)
+      #contents += "\n#{InstallationDirectory}/" unless merged_folder&.last
 
       File.open(gitignore_file, 'w') { |file| file.puts contents }
     end
